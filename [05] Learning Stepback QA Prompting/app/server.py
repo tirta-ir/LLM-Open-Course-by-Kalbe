b24d221bd79b6c2df8b5e5b3b92e23bf28358b29
Miftahul Tirta Irawan <miftahul.tirta@gmail.com>
2024-04-01 15:46:09 +0700

    Add files via upload

---
+++ [05] Learning Stepback QA Prompting/app/server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from stepback_qa_prompting.chain import chain as stepback_qa_prompting_chain
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+# Edit this to add the chain you want to add
+add_routes(app, stepback_qa_prompting_chain, path = "/stepback-qa-prompting")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
